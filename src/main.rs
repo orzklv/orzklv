d5a1673f86c96f310bc7732253664b993fbe8bc1
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-02-18 04:29:06 +0500

    fix: path to konata's asset

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-use image::imageops::FilterType;
 use image::ImageReader;
+use image::imageops::FilterType;
 use std::io::Cursor;
 use tapciify::{
     AsciiArtConverter, AsciiArtConverterOptions, CustomRatioResize, DEFAULT_FONT_RATIO,
 };
 
 fn main() {
-    let img = ImageReader::new(Cursor::new(include_bytes!("../assets-anime/Konata.webp")))
-        .with_guessed_format()
-        .expect("Couldn't read image format")
-        .decode()
-        .expect("Failed to decode image");
+    let img = ImageReader::new(Cursor::new(include_bytes!(
+        "../past-assets/assets-anime/Konata.webp"
+    )))
+    .with_guessed_format()
+    .expect("Couldn't read image format")
+    .decode()
+    .expect("Failed to decode image");
 
     let result = img
         .resize_custom_ratio(Some(64), None, DEFAULT_FONT_RATIO, FilterType::Triangle)
