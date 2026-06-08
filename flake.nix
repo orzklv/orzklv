5ed9f5901e6addb7429a4f0f4d81ba9f30839f28
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-06-09 03:55:14 +0500

    chore: remove other targets

--- flake.nix
+++ flake.nix
@@ -17,12 +17,7 @@
     flake-parts.lib.mkFlake { inherit inputs; } (
       { ... }:
       {
-        systems = [
-          "aarch64-darwin"
-          "x86_64-darwin"
-          "aarch64-linux"
-          "x86_64-linux"
-        ];
+        systems = [ "x86_64-linux" ];
         perSystem =
           { pkgs, ... }:
           {
